a3cca641e9960253d3ed5b47137690cbfa087b72
NSCoder8384 <mamtakri8384@gmail.com>
2026-05-12 22:32:27 +0530

    Added function.js

--- function.js
+++ function.js
@@ -1,4 +1,20 @@
-function iseven( number){
-    if (number % 2 === 0){
-        return true;
-    } 
+// // function iseven( number){
+// //     if (number % 2 === 0){
+// //       console.log(number + ' is an even number'); 
+// //     }
+// //     else{
+// //       console.log(number + ' is an odd number');    
+// //     }
+
+//     } 
+//     iseven(4);  
+//     iseven(7);  
+
+    function iseven(number){
+        if (number % 2 === 0){
+          console.log(number + ' is an even number'); 
+        }
+        else{
+          console.log(number + ' is an not even number');    
+        }
+     }
